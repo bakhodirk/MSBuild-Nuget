378ff5137cfbe1437fc596fa0ab65a81ca3e3f02
Bakhodir Kurbanov <bahodir.qurbonov@gmail.com>
2017-11-11 16:41:53 +0500

    Added NuPAck task

--- MSBuild.Nuget.Test/Program.cs
+++ MSBuild.Nuget.Test/Program.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MSBuild.Nuget
+namespace MSBuild.Nuget.Test
 {
-    public class Class1
+    class Program
     {
-
+        static void Main(string[] args)
+        {
+        }
     }
 }
---
+++ MSBuild.Nuget/NuPack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace MSBuild.Nuget
+{
+    public class NuPack : Task
+    {
+        public override bool Execute()
+        {
+            Log.LogMessage("NuPack Message");
+            Log.LogWarning("NuPack Warning");
+            Log.LogError("NuPack Error");
+            return true;
+        }
+    }
+}
